5a6bc2cef9ebd64a4d9ca6db166fbfd1558f5a2a
Eduardo Coelho <eduardo13ccoelho@gmai.com>
2021-06-30 09:36:55 -0300

    Revisão Lógica JS

---
+++ Revisão-Lógica-Programação/variaveis.js
@@ -0,0 +1,14 @@
+// var, let, const
+
+// var -> ES5 - cross-browser
+// let e const -> ES2015
+
+let teste="minha string"
+teste=10
+
+const teste2="eu sou imutavel"
+
+let teste3
+teste3=30
+
+console.log(teste3)
