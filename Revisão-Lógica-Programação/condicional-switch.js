ce605f04f35ce430ba396b002bb61258749da32d
Eduardo Coelho <eduardo13ccoelho@gmai.com>
2021-06-30 15:26:42 -0300

    Revisão Lógica JS

---
+++ Revisão-Lógica-Programação/condicional-switch.js
@@ -0,0 +1,18 @@
+let diaSemana=3
+if(diaSemana===0) {
+    console.log("hoje é domingo")
+} else if(diaSemana===1){
+    console.log("hoje é segunda")
+} else if(diaSemana===2){
+    console.log("hoje é terça")
+} else if(diaSemana===3){
+    console.log("hoje é quarta")
+} else if(diaSemana===4) {
+    console.log("hoje é quinta")
+} else if(diaSemana===5) {
+    console.log("hoje é sexta")
+} else if(diaSemana===6) {
+    console.log("hoje é sábado")
+} else {
+    console.log("hoje é --")
+}
