2f941509fc8687761b0cffdeb4657d7fe3159360
Eduardo Coelho <eduardo13ccoelho@gmai.com>
2021-07-02 09:58:51 -0300

    Introdução

---
+++ Introdução/primeiro-exemplo.js
@@ -0,0 +1,14 @@
+console.log("JavaScript funcionando 2222")
+;
+/*
+let nome="Eduardo Coelho"
+console.log(10+20)
+*/
+;
+console.log("qualquer coisa")
+;
+// uma unica linha
+;
+let nome ="Eduardo"; alert(nome)
+;
+const hasAttrubite=""
